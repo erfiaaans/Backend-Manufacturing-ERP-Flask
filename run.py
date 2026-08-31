acefc663fec34dde3136aca9abaec4efc8b4a349
erfiaaans <erfianadia05@gmail.com>
2026-08-31 20:17:07 +0700

    feat: add test adn setup API

--- run.py
+++ run.py
@@ -1,10 +1,13 @@
-from flask import Flask
-app = Flask(__name__)
-@app.get("/")
-def home():
-    return{
-        "message": "Manufacturing ERP API is running"
-    }
+# from flask import Flask
+# app = Flask(__name__)
+# @app.get("/")
+# def home():
+#     return{
+#         "message": "Manufacturing ERP API is running"
+#     }
+from backend_manufacturing_erp_flask import create_app
+
+app = create_app()
 if __name__ == "__main__":
     app.run(debug=True)
     
--- src/backend_manufacturing_erp_flask/__init__.py
+++ src/backend_manufacturing_erp_flask/__init__.py
@@ -1,2 +1,25 @@
-def main() -> None:
-    print("Hello from backend-manufacturing-erp-flask!")
+from flask import Flask
+from .config import Config 
+from .extensions import db, migrate, jwt
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    db.init_app(app)
+    migrate.init_app(app, db)
+    # jwt.init_app(app)
+    @app.get("/api/health")
+    def health():
+        return {
+            "status": "success",
+            "message": "Manufacturing ERP API is running"
+        }
+    @app.get("/api/health/db")
+    def database_health():
+        from sqlalchemy import text
+        result = db.session.execute(text("SELECT 1"))
+        return{
+            "status": "success",
+            "database": result.scalar()
+        }
+    return app
